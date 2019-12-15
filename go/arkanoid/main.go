fdb0069787d7747992735c85456e727b09827a02
Serialdev <carlos.mariscal.melgar@gmail.com>
2019-12-16 01:35:33 +0200

    [+] Ebiten canary and main arkanoid project

---
+++ go/arkanoid/main.go
@@ -0,0 +1,23 @@
+package main
+
+
+
+import (
+    "github.com/hajimehoshi/ebiten"
+    "github.com/hajimehoshi/ebiten/ebitenutil" // This is required to draw debug texts.
+)
+
+
+
+func update(screen *ebiten.Image) error {
+	ebitenutil.DebugPrint(screen, "first ebiten game!");
+	return nil;
+}
+
+func main() {
+	if err := ebiten.Run(update, 320, 240, 2, "Hello World!");
+	err != nil{
+		panic(err);
+	}
+}
+
