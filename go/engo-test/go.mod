e19c123c7a467271d27a74cff074729d0c6a09b1
Serialdev <carlos.mariscal.melgar@gmail.com>
2019-12-23 01:31:19 +0200

    [+] engo test-dump

---
+++ go/engo-test/go.mod
@@ -0,0 +1,9 @@
+module main
+
+go 1.13
+
+require (
+	github.com/EngoEngine/ecs v1.0.3
+	github.com/EngoEngine/engo v1.0.4
+	github.com/hajimehoshi/ebiten v1.10.1 // indirect
+)
---
+++ go/engo-test/main.go
@@ -0,0 +1,19 @@
+package main
+
+
+
+import (
+	"github.com/EngoEngine/engo"
+	"fmt"
+	"log"
+	"bytes"
+	"image"
+	resources "github.com/hajimehoshi/ebiten/examples/resources/images/flappy"
+	"github.com/EngoEngine/ecs"
+
+)
+
+
+func main() {
+}
+
