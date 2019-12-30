93e2c61e07fb3dc9978140192b2b45084fb43975
serialdev <carlos.mariscal.melgar@gmail.com>
2019-12-30 21:19:38 +0000

    [+] zig modifications

--- zig/kruskall/main.zig
+++ zig/kruskall/main.zig
@@ -3,19 +3,24 @@ const io = @import("std").io;
 
 const assert = std.debug.assert;
 
-const edge = struct{
+const Edge = struct{
     src: i32,
     dest: i32,
     weight: i32,
 };
 
-const graph = struct{
+const Graph = struct{
     v: i32,
     e: i32,
 
-    edges: []edge,
+    edges: []Edge,
 };
 
+fn createEdge(e:i32) Edge {
+    var edge: Edge = Edge{.src = e, .dest = 0, .weight = 0};
+    return edge;
+    
+} 
 test "kruskal" {
     const V  = 4;
     std.debug.warn("first one:\n", V);
@@ -24,4 +29,10 @@ test "kruskal" {
 pub fn main()  !void {
     const stdout = &std.io.getStdOut().outStream().stream;
     try stdout.print("Hello, {}!\n", .{"world"});
+
+    const e1 = createEdge(5);
+    // const e1 = Struct{};
+    try stdout.print("Hello, {}!", .{@typeId( e1)});
 }
+
+
